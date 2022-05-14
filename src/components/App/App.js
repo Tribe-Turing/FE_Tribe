9ad4dc261688ed20396edcc3c4c0624b157bda13
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-13 19:41:53 -0600

    Add dummy data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,11 @@ import React, {useState, useEffect} from 'react';
 import Dashboard from '../Dashboard/Dashboard';
 
 function App() {
-  const [users, setUSers] = useState([]);
+  const [users, setUsers] = useState([
+    {username: 'Rory', id: 1, image: 'example.jpg'},
+    {username: 'Lauralyn', id: 2, image: 'example2.jpg'},
+    {username: 'Andrew', id: 3, image: 'example3.jpg'}
+  ]);
   const [userID, setUserID] = useState(0);
   return (
     <div>
