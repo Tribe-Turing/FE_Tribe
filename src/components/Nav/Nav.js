b498aa9845288cb109c4377161b719d9ac1b34d1
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-13 19:46:08 -0600

    Add basic nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav>
+            <button>Friends</button>
+            <button>Chat</button>
+            <button>Other</button>
+        </nav>
+    )
+}
+
+export default Nav;
