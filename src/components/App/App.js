8dbf2f32fbd6d4f0da8f9720ec35963c1c44f41b
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-13 19:20:46 -0600

    Add component structure to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Dashboard from '../Dashboard/Dashboard';
 
 function App() {
+  const [users, setUSers] = setState([]);
+  const [userID, setUserID] = setState(0);
   return (
     <div>
+      <Dashboard />
     </div>
   );
 }
