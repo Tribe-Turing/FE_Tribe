ee0211dbdba368085264e39d43c8894b27d36d68
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-15 17:59:13 -0600

    Add router link to all user cards

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './UserCard.css'
 
 const UserCard = ({username, id}) => {
     return (
+        <Link to='/user'>
         <div className='user-card' key={id}>
             <p>{username}</p>
         </div>
+        </Link>
     )
 }
 
