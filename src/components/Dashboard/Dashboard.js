ea5cbe12d72d50d3889f6d575de5cd5cc9cb45f2
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-15 13:13:07 -0600

    Add functionality to filter by interests

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserCard from '../UserCard/UserCard';
 import InterestSearchForm from '../InterestSearchForm/InterestSearchForm';
+import UserProfile from '../UserProfile/UserProfile';
 
 const Dashboard = ({users}) => { 
-    const getUserDetails = () => {
-
+    const [currentInterest, setCurrentInterest] = useState('')
+    const filterInterests = (interest) => {
+        setCurrentInterest(interest)
     }
+    const filteredUsers = users.filter(user => {
+        return user.interests.includes(currentInterest)
+    }).map(user => {
+        return(
+            <UserCard 
+                id={user.id}
+                key={user.id}
+                username={user.first_name}
+            />)
+    })
     const userCards = users.map(user => {
         return(
             <UserCard 
@@ -14,11 +26,11 @@ const Dashboard = ({users}) => {
                 username={user.first_name}
             />)
     })
-
     return (
         <div>
-            <InterestSearchForm />
-            {userCards}
+            <InterestSearchForm filterInterests={filterInterests}/>
+            {currentInterest ? filteredUsers : userCards} 
+            <UserProfile />
         </div>
     )
 }
