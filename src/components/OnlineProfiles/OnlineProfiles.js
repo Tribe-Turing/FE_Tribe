f286f7d68e364c7b11259cf8cf1ba9e55504fa83
Sam Mistele <smmstl0@gmail.com>
2022-05-16 17:07:41 -0600

    Comment out iterative functionality for routes

--- src/components/OnlineProfiles/OnlineProfiles.js
+++ src/components/OnlineProfiles/OnlineProfiles.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import UserProfile from '../UserProfile/UserProfile';
 
-const OnlineProfiles = ({users}) => { 
+const OnlineProfiles = ({users}) => {
     // Will need filter for online profiles, but otherwise will work the same
-    const userProfiles = users.map(user => {
-        return(
-            <UserProfile
-                image={user.image}
-                id={user.id}
-                key={user.id}
-                username={user.username}
-            />)
-    })
 
     return (
         <div>
-            {userProfiles}
+            it profiles
         </div>
     )
 }
 
 export default OnlineProfiles;
+
+
+// const userProfiles = users.map(user => {
+  //     return(
+    //         <UserProfile
+    //             image={user.image}
+    //             id={user.id}
+    //             key={user.id}
+    //             username={user.username}
+    //         />)
+    // })
