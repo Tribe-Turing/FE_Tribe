d37b36f5401e9e6342ea6fcb2b7c90479751fcd2
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-17 19:01:44 -0600

    Add test for friends button

--- cypress/integration/Nav_spec.js
+++ cypress/integration/Nav_spec.js
@@ -18,4 +18,11 @@ describe('Nav', () => {
         .click()
         .url().should('eq', 'http://localhost:3000/chatlist')
     })
+    it('As a user, I should be able to click the friends button to go back to the dashboard', () => {
+        cy.get('button[class="friends-button"]')
+        .should('contain', 'Friends')
+        .should('be.visible')
+        .click()
+        .url().should('eq', 'http://localhost:3000/')
+    })
 })
