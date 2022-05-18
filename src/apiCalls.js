fb16aaf43a56f91ee37d5d2dd9ba0b439894acc7
Rory Magee <roryemagee@gmail.com>
2022-05-17 20:13:49 -0600

    Add fetchOneUser function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,6 +2,10 @@ const apiCalls = {
   fetchUsers() {
     return fetch(`https://be-tribe.herokuapp.com/api/v1/users`)
       .then(response => response.json())
+  },
+  fetchOneUser(id) {
+    return fetch(`https://be-tribe.herokuapp.com/api/v1/users/${id}`)
+      .then(response => response.json())
   }
 }
 
