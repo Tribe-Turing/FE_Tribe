bd1a95c5e81026cebfbd97a1f37d4862f6f62a03
Rory Magee <roryemagee@gmail.com>
2022-05-19 15:43:39 -0600

    Add images to profiles and profile cards

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './UserCard.css'
 
-const UserCard = ({username, id}) => {
+const UserCard = ({username, id, image}) => {
     return (
         <Link to={`/${id}`}>
         <div className='user-card' key={id}>
+            <img src={image}/>
             <p>{username}</p>
         </div>
         </Link>
