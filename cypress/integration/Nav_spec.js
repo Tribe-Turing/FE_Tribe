32312a732580ba4b953841c698e9306d7607ea31
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-20 15:14:56 -0600

    Add stubs to Nav tests

--- cypress/integration/Nav_spec.js
+++ cypress/integration/Nav_spec.js
@@ -1,16 +1,19 @@
 describe('Nav', () => {
     it('As a User, I should see the other button', () => {
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
         cy.visit('http://localhost:3000/')
         .get('button[class="settings-nav-button"]')
         .should('contain', 'Other')
         .should('be.visible')
     })
     it('As a user, I should be able to click the settings button', ()=> {
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
         cy.visit('http://localhost:3000/')
         .get('button[class="settings-nav-button"]').click()
         .url().should('eq', 'http://localhost:3000/settings')
     })
     it('As a user, I should see a chat button and be able to click it', () => {
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
         cy.visit('http://localhost:3000/')
         .get('button[class="chat-button"]')
         .should('contain', 'Chat')
@@ -19,6 +22,7 @@ describe('Nav', () => {
         .url().should('eq', 'http://localhost:3000/chatlist')
     })
     it('As a user, I should be able to click the friends button to go back to the dashboard', () => {
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
         cy.get('button[class="friends-button"]')
         .should('contain', 'Friends')
         .should('be.visible')
