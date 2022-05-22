23f55246ab801a87507241baacbca49bfbee8bba
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-22 11:28:29 -0600

    Change wording from fixtures to fixture in Nav

--- cypress/integration/Nav_spec.js
+++ cypress/integration/Nav_spec.js
@@ -1,28 +1,19 @@
 describe('Nav', () => {
     it('As a User, I should see the other button', () => {
-<<<<<<< HEAD
-        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
-=======
->>>>>>> d6518bc9ab8f58d60635f55aa2a2af26511fa7e0
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixture: 'allUsers.json'})
         cy.visit('http://localhost:3000/')
         .get('button[class="settings-nav-button"]')
         .should('contain', 'Other')
         .should('be.visible')
     })
     it('As a user, I should be able to click the settings button', ()=> {
-<<<<<<< HEAD
-        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
-=======
->>>>>>> d6518bc9ab8f58d60635f55aa2a2af26511fa7e0
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixture: 'allUsers.json'})
         cy.visit('http://localhost:3000/')
         .get('button[class="settings-nav-button"]').click()
         .url().should('eq', 'http://localhost:3000/settings')
     })
     it('As a user, I should see a chat button and be able to click it', () => {
-<<<<<<< HEAD
-        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
-=======
->>>>>>> d6518bc9ab8f58d60635f55aa2a2af26511fa7e0
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixture: 'allUsers.json'})
         cy.visit('http://localhost:3000/')
         .get('button[class="chat-button"]')
         .should('contain', 'Chat')
@@ -31,10 +22,7 @@ describe('Nav', () => {
         .url().should('eq', 'http://localhost:3000/chatlist')
     })
     it('As a user, I should be able to click the friends button to go back to the dashboard', () => {
-<<<<<<< HEAD
-        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixtures: 'allUsers.json'})
-=======
->>>>>>> d6518bc9ab8f58d60635f55aa2a2af26511fa7e0
+        cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixture: 'allUsers.json'})
         cy.get('button[class="friends-button"]')
         .should('contain', 'Friends')
         .should('be.visible')
