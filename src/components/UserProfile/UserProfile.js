d387395c584ba4902b58fcf73674b0a370573a7e
Dillon Parker <prkr93@gmail.com>
2022-05-23 09:43:19 -0600

    Add setMessageUser to determine whether a fetch call can be ran immediately

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import apiCalls from '../../apiCalls';
 import loadingSpinner from '../../assets/loadingSpinner.gif';
+import {Redirect, useHistory} from 'react-router-dom';
 
-const UserProfile = ({id}) => {
+const UserProfile = ({id, loggedInUser, setMessageUser}) => {
+    const history = useHistory();
     const [isLoading, setLoading] = useState(true);
     const [details, setDetails] = useState({});
 
@@ -17,6 +19,30 @@ const UserProfile = ({id}) => {
         getOneUser();
     }, []);
 
+    const messageUser = async () => {
+      const foundConvo = loggedInUser.conversations.find(conversation => conversation.convo.user_a_id == details.id || conversation.convo.user_b_id == details.id);
+      if (foundConvo) {
+        setMessageUser('')
+        return history.push(`/conversations/${foundConvo.convo.id}`)
+      }
+      else {
+        const res = await apiCalls.fetchConversations();
+        const nextID = await res.length + 1;
+
+
+        const data = await {
+          "user_a_id": loggedInUser.id,
+          "user_b_id": details.id,
+          "id": nextID
+        }
+
+        const response = await apiCalls.createConversation(data)
+
+        history.push(`/conversations/${nextID}`)
+      }
+    }
+
+
     return (
         isLoading ? <img className="loading-spinner" src={loadingSpinner}/> :
         <div>
@@ -27,7 +53,7 @@ const UserProfile = ({id}) => {
             <ul>
             {details.interests.map((interest, index) => <li key={index}>{interest}</li>)}
             </ul>
-            <button>Message {details.first_name}</button>
+            <button onClick={() => messageUser()}>Message {details.first_name}</button>
         </div>
     )
 }
