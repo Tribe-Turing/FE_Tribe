5108fa86fcd841f2eb31f6106bdfbfceb2126b40
Dillon Parker <prkr93@gmail.com>
2022-05-23 09:40:19 -0600

    Add fetch calls for createConversation and fetchConversations

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,6 +6,17 @@ const apiCalls = {
   fetchOneUser(id) {
     return fetch(`https://be-tribe.herokuapp.com/api/v1/users/${id}`)
       .then(response => response.json())
+  },
+  createConversation(data) {
+    return fetch(`https://be-tribe.herokuapp.com/api/v1/conversations`, {
+      method: "POST",
+      headers: {"content-type":"application/json"},
+      body: JSON.stringify(data)
+    })
+  },
+  fetchConversations() {
+    return fetch(`https://be-tribe.herokuapp.com/api/v1/conversations`)
+      .then(response => response.json())
   }
 }
 
