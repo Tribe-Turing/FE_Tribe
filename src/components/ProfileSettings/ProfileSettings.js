953ea03fdea1b22155d6d651e0bf93c652b86127
lswatson16 <lauralynw16@gmail.com>
2022-05-24 08:28:26 -0400

    Remove unused code

--- src/components/ProfileSettings/ProfileSettings.js
+++ src/components/ProfileSettings/ProfileSettings.js
@@ -14,9 +14,6 @@ const ProfileSettings = ({ loggedInUser }) => {
       ...profileSettings, [e.target.name]: e.target.value
     }))
 
-    if(e.target.name === 'pronouns') {
-
-    }
   }
 
   // const [location, setLocation] = useState('')
