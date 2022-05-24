fb46015b5ef34eb6c0fad065e93ecb5ad2547872
Andrew Musselman <andrew.mussels@gmail.com>
2022-05-24 16:22:15 -0600

    Refactor test to work with new form layout

--- cypress/integration/ProfileSettings_spec.js
+++ cypress/integration/ProfileSettings_spec.js
@@ -2,6 +2,7 @@ describe('Profile Settings', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://be-tribe.herokuapp.com/api/v1/users', {fixture: 'allUsers.json'})
     const url = 'http://localhost:3000/settings'
+    cy.visit('http://localhost:3000/login/1')
     cy.visit(url)
   })
 
@@ -19,76 +20,54 @@ describe('Profile Settings', () => {
   it('should display the title/heading of profile settings view', () => {
     cy.get('h1')
       .should('be.visible')
-      .contains('Edit Your Profile')
+      .should('contain', 'Edit Your Profile')
   })
   it('should display a form to update the location, pronouns, interests, and bio', () => {
     cy.get('form')
       .should('be.visible')
       .within(form => {
         cy.get('label').should('have.length', 17).should('be.visible')
-        cy.get('input').should('have.length', 17).should('be.visible')
+        cy.get('input').should('have.length', 16).should('be.visible')
         cy.get('button').should('have.length', 1).should('be.visible')
       })
 
-    cy.get('label').first().contains('Change location')
-    cy.get('#location').should('have.attr', 'placeholder').and('contains', 'Denver, CO')
+    cy.get('label').first().contains('Location: ')
+    cy.get('#city').should('have.attr', 'placeholder').and('contains', 'Denver, CO')
     
-    cy.get('form > :nth-child(3)')
+    cy.get('form > :nth-child(2)')
       .contains('Would you like to update your pronouns?')
+      cy.get('div[class="pronoun-radio"]')
+      .should(($input) => {
+        expect($input).to.have.length(5)
+        expect($input.eq(0)).to.contain('she/her')
+        expect($input.eq(1)).to.contain('he/him')
+        expect($input.eq(2)).to.contain('they/them')
+        expect($input.eq(3)).to.contain('ze/zie')
+        expect($input.eq(4)).to.contain('name')
+    })
+  
+    cy.get('#bio')
+    .should('have.attr', 'placeholder', 'What do you want to bring to tribe?')
+      .contains('self proclaimed cool kid from the block')
 
-    cy.get('form > :nth-child(5)')
-      .contains('she/her')
-
-    cy.get('form > :nth-child(7)')
-      .contains('he/him')
-
-    cy.get('form > :nth-child(9)')
-      .contains('they/them')
-
-    cy.get('form > :nth-child(11)')
-      .contains('ze/zie')
-
-    cy.get('form > :nth-child(13)')
-      .contains('name')
-
-    cy.get(':nth-child(14)')
-      .contains('Would you like to update your interests?')
-
-    cy.get('form > :nth-child(16)')
-      .contains('Sports')
-    
-    cy.get('form > :nth-child(18)')
-      .contains('Nature')
-
-    cy.get('form > :nth-child(20)')
-      .contains('Music')
-
-    cy.get('form > :nth-child(22)')
-      .contains('Night Life')
-
-    cy.get('form > :nth-child(24)')
-      .contains('Art')
-
-    cy.get('form > :nth-child(26)')
-      .contains('Cinema')
-
-    cy.get('form > :nth-child(28)')
-      .contains('Food')
-
-    cy.get('form > :nth-child(30)')
-      .contains('Video Games')
-
-    cy.get('form > :nth-child(32)')
-      .contains('Traveling')
-
-    cy.get('form > :nth-child(34)')
-      .contains('Networking')
-
-    cy.get('label').last().contains('Bio')
+    cy.get('div[class="interest-container"]')
+      .should(($input) => {
+        expect($input).to.have.length(10)
+        expect($input.eq(0)).to.contain('Sports')
+        expect($input.eq(1)).to.contain('Nature')
+        expect($input.eq(2)).to.contain('Music')
+        expect($input.eq(3)).to.contain('Night Life')
+        expect($input.eq(4)).to.contain('Art')
+        expect($input.eq(5)).to.contain('Cinema')
+        expect($input.eq(6)).to.contain('Food')
+        expect($input.eq(7)).to.contain('Gaming')
+        expect($input.eq(8)).to.contain('Traveling')
+        expect($input.eq(9)).to.contain('Networking')
+      })
 
     cy.get('.submit-button')
       .should('be.visible')
-      .contains('Submit')
+      .contains('Save Changes')
   })
   it('should be able to submit a form to update the location, pronouns, interests, and bio of a user', () => {
 
