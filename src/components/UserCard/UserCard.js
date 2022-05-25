5ddac7eb9f9f2f89de91e9e47e212ca15432c803
Rory Magee <roryemagee@gmail.com>
2022-05-25 11:53:32 -0600

    Fix typo in previous alt tag

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -7,7 +7,7 @@ const UserCard = ({username, id, image, setMessageUser}) => {
     return (
         <Link to={`/user/${id}`}>
         <div className='user-card' key={id} onClick={() => setMessageUser(id)}>
-            <img className="photo" src={animals[image] alt="Animal Profile Icon"}/>
+            <img className="photo" src={animals[image]} alt="Animal Profile Icon" />
             <div className='name-banner'>
               <p className='username'>{username}</p>
             </div>
